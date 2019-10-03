cc85082185d963e42d7fe96c0c8b50a1d623b29e
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-10-03 14:31:36 -0400

    update sha

--- example.rb
+++ example.rb
@@ -2,7 +2,7 @@ class Example < Formula
     desc "for each dir do bash."
     homepage "https://github.com/MichaelDimmitt/homebrew-taps"
     url "https://github.com/MichaelDimmitt/homebrew-taps/releases/download/1.0.0/example-1.0.0.zip"
-    sha256 "254086751cfed11f3a067036562234a80ea7169d5ef6890fa14b96827992bfde"
+    sha256 "f79eb924a81c50522d6c1f5cd8b20bb3d10f83098a0381d495f50c45fc65ae3e"
 
 
     def install
